c58ee26aa3a0d3544f504088227c0f7f63e23736
Maithili-Bagwan <bagwanmaithili@gmail.com>
2025-08-02 23:07:38 +0530

    Create Opp1.java
    Basic concept of class and its constructor.

---
+++ Opp1.java
@@ -0,0 +1,10 @@
+public class Opp1 {
+    int x;
+    public Opp1(int y){
+        x=y;
+    }
+    public static void main(String[] args) {
+        Opp1 obj = new Opp1(10);
+        System.out.println(obj.x);
+    }
+}
