59bd996bf6b20baab39e4e0cbcd45cdeb912cd89
Maithili-Bagwan <bagwanmaithili@gmail.com>
2025-08-03 23:35:34 +0530

    Create Main.java
    understanding the concept of data types in java.

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String [] args)
+    {
+        System.out.println("Hello World");
+        String name ="John";
+        int a = 12;
+        float f = 13.5f;
+        char c ='X';
+        boolean b= true;
+        System.out.println("Name " +name);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(f);
+    }
+}
